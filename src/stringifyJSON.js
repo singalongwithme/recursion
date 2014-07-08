dce589524b252807d12f3d74f435eeabf7bd164a
singalongwithme <csinghavong@gmail.com>
2014-07-07 23:00:54 -0700

    initial commit, not completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,28 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj == "number"){
+    return obj.toString();
+  }
+  else if(obj == null){
+    return 'null';
+  }
+  else if(typeof obj == "boolean"){
+    return obj.toString();
+  }
+  else if(typeof obj == "string"){
+    return "\"" + obj + "\"";
+  }
+  else if(typeof obj == "object"){
+  var string = "";
+  var bracket = "{";
+  var bracketEnd = "}";
+  for(var key in obj){
+    if(typeof obj[key] !== "string"){
+    string += "\"" + key + "\":" + obj[key] + ",";
+    } else {
+    string += "\"" + key + "\":\"" + obj[key] + "\","; }
+    }
+  string = string.slice(0,-1);
+  return bracket.concat(string, bracketEnd); }
 };
